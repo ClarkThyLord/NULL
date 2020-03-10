b3eb9aed11c5e3a6820d3f077b1eef6368ffb8ff
FrogEmperor <43252660+FrogEmperor@users.noreply.github.com>
2020-03-10 16:53:40 -0700

    Comments on player code

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,18 +10,19 @@ export (float) var ACCELERATION
 export (float) var jump_height
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	#Get the scene's camera
 	camera = get_viewport().get_camera().get_global_transform()
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func _process(delta):
-	walk(delta)
+	#Call all the action the player can do
+	movement(delta)
+	attack()
+	
+func attack():
+	#Enable collision box when input attack is done and disable it when not
 	if(Input.is_action_pressed("attack")):
 		get_node("Hit").get_node("Hitbox").disabled = false
 	else:
@@ -29,9 +30,9 @@ func _process(delta):
 
 
 		
-func walk(delta):
+func movement(delta):
+	#Declare the vector that will tell the direction the player wants to move
 	var dir = Vector3()
-	
 	if(Input.is_action_pressed("move_fw")):
 		dir.z -= 1
 	if(Input.is_action_pressed("move_bw")):
@@ -42,32 +43,40 @@ func walk(delta):
 		dir.x += 1
 	dir = dir.normalized()
 	
+	#Make gravity change y position of player
 	velocity.y += gravity * delta
 	
+	#Declare a temporal variable for velocity so that the y vector doesnt get change
 	var temp_velocity = velocity
 	temp_velocity.y = 0
 	
+	#See if player wants the character to run and with that get the new position of the object
 	var speed = SPEED
 	if(Input.is_action_pressed("action_shift")):
 		speed = RUN_SPEED
 	var target = dir * speed
 	
+	#deaccelerate when player is changing direction or stopping
 	var accel = DE_ACCELERATION
 	if(dir.dot(temp_velocity)>0):
 		accel = ACCELERATION
-	temp_velocity = temp_velocity.linear_interpolate(target, DE_ACCELERATION * delta)
+	
+	#Call the linear_interpolate to make move_and_slide movement with the acceleration given 
+	temp_velocity = temp_velocity.linear_interpolate(target, accel * delta)
 	velocity.x = temp_velocity.x
 	velocity.z = temp_velocity.z
-	
 	velocity = move_and_slide(velocity, Vector3(0,1,0))
 
+	#Rotate the player towards where its moving, "if" dectects if player is moving
 	if abs(velocity.x) >= 1 or abs(velocity.z) >= 1:
 		var rot = get_rotation()
 		rot.y = atan2(-temp_velocity.x,-temp_velocity.z)
 		set_rotation(rot)
 	
+	#Jumping when player its on floor and inpus space is done
 	if is_on_floor() and Input.is_action_pressed("action_space"):
 		velocity.y = jump_height
 
+#Attack hit
 func _on_Hit_body_entered(body):
 	print("hit")
