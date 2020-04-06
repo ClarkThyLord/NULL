aa29f7c13ef314edc93cc37d4894380b13157bc1
Christian Cuevas <ccpotential@gmail.com>
2020-04-05 19:15:43 -0700

    correct FrogEmperor movement
    Clark was here :V

--- entities/FrogEmperor/FrogEmperor.gd
+++ entities/FrogEmperor/FrogEmperor.gd
@@ -3,11 +3,13 @@ extends KinematicBody
 class_name FrogEmperor
 
 
+
 # Refrences
 onready var CameraPivot := get_node("CameraPivot")
-onready var Camera := get_node("CameraPivot/Camera")
+onready var PlayerCamera := get_node("CameraPivot/PlayerCamera")
 
 onready var HitArea := get_node("HitArea")
+
 
 
 # Declarations
@@ -17,11 +19,13 @@ export(int, 0, 100, 1) var Stamina := 100
 export(int, 0, 100, 1) var Speed := 25
 export(float, 0, 10, 0.01) var SpeedBoost := 1.75
 
+var jumps := 0
 export(int, 0, 3, 1) var MaxJumps := 1
-export(int, 0, 10, 1) var JumpHeight := 3
-export(float, 0, 100, 0.1) var Gravity := 10
+export(int, 0, 100, 1) var JumpHeight := 3
+export(int, 0, 100, 1) var Gravity := 10
 
 export(int, 0, 100, 1) var MouseSensitivity := 10
+
 
 
 # Core
@@ -31,17 +35,6 @@ func _ready() -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
-func attack() -> void:
-	pass
-
-func move(direction : Vector3, delta : float) -> void:
-	move_and_slide(Vector3.DOWN * Gravity)
-	move_and_slide(direction * Speed)
-
-
-func _process(delta : float) -> void:
-	pass
-
 func _physics_process(delta):
 	if not Engine.editor_hint:
 		var direction := Vector3()
@@ -49,9 +42,19 @@ func _physics_process(delta):
 		if Input.is_action_pressed("move_back"): direction += Vector3.BACK
 		if Input.is_action_pressed("move_right"): direction += Vector3.RIGHT
 		if Input.is_action_pressed("move_left"): direction += Vector3.LEFT
-		if Input.is_action_pressed("move_boost"): direction *= SpeedBoost
-		if Input.is_action_just_pressed("move_jump"): direction += Vector3.UP
-		move(direction, delta)
+		
+		var velocity := Vector3()
+		velocity += CameraPivot.global_transform.basis.x * direction.x
+		velocity += CameraPivot.global_transform.basis.z * direction.z
+		velocity *= Speed * (SpeedBoost if Input.is_action_pressed("move_boost") else 1)
+		
+		if jumps < MaxJumps and Input.is_action_just_pressed("move_jump"):
+			jumps += 1
+			translate(Vector3.UP * JumpHeight)
+		elif is_on_floor(): jumps = 0
+		
+		move_and_slide(velocity, Vector3.UP)
+		move_and_slide(Vector3.DOWN * Gravity, Vector3.UP)
 
 func _unhandled_input(event : InputEvent):
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
@@ -61,7 +64,7 @@ func _unhandled_input(event : InputEvent):
 		elif event is InputEventMouseButton:
 			match event.button_index:
 				BUTTON_WHEEL_UP, BUTTON_WHEEL_DOWN:
-					Camera.translation += Vector3(0, 0.1, 0.1) * (1 if event.button_index == BUTTON_WHEEL_UP else -1)
+					PlayerCamera.translation += Vector3(0, 0.1, 0.1) * (1 if event.button_index == BUTTON_WHEEL_DOWN else -1)
 
 func _unhandled_key_input(event : InputEventKey) -> void:
 	if not event.pressed:
