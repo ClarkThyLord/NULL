9244e52d80b8c1536bbf01e1e583df5c4e5f0607
FrogEmperor <43252660+FrogEmperor@users.noreply.github.com>
2020-04-06 21:40:16 -0700

    Pawn
    Health bar, movement, attack

--- entities/enemies/Enemy.gd
+++ entities/enemies/Enemy.gd
@@ -1,13 +1,15 @@
 extends Entity
 class_name Enemy
-
-
+onready var FrogEmperor = get_parent().get_node("FrogEmperor")
 
 # Core
 func _ready():
 	add_to_group("enemies", true)
-
-
+	
+func _process(delta):
+	look_at(FrogEmperor.get_transform().origin, Vector3(0,1,0))
+	rotation.z = 0
+	rotation.x = 0
 # Seek Schema -> {
 #	'found' : null | Entity
 #}
--- entities/enemies/Pawn/Pawn.gd
+++ entities/enemies/Pawn/Pawn.gd
@@ -1,13 +1,34 @@
 extends Enemy
 class_name Pawn
+export (int) var speed
+export (int) var hitRate
 
-
-
+var hitFrames = 0
 # Core
 func _ready():
 	add_to_group("pawns", true)
 
+func set_health(health : int) -> void:
+	get_node("Viewport").get_node("Health").value = health
+	.set_health(health)
 
+func _process(delta):
+	hitFrames = (hitFrames + 1) % hitRate
+	if hitFrames == 0:
+		attack()
+	else:
+		get_node("HitArea").get_node("HitBox").disabled = true
+	
+	var global_direction = Vector3(0,0,1)
+	var local_direction = global_direction.rotated(Vector3(0,1,0), rotation.y)
+	var velocity = - local_direction * speed
+	move_and_slide(velocity)
+	print(Health)
+	
+
+func attack():
+	get_node("HitArea").get_node("HitBox").disabled = false
+	
 func _on_HitArea_body_entered(body):
 	if body is Entity:
 		body.hurt({
