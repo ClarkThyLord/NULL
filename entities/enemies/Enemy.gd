ddfaf6bdc3d8e3175467309b4edad4d5fd1bf31e
Christian Cuevas <ccpotential@gmail.com>
2020-04-07 12:43:40 -0700

    fixed enemy seek
    Clark was here :V

--- entities/enemies/Enemy.gd
+++ entities/enemies/Enemy.gd
@@ -4,9 +4,9 @@ extends "res://entities/Entity.gd"
 
 # Declarations
 var Target : Spatial
-export(NodePath) var TargetPath : NodePath setget set_target
-func set_target(targetpath : NodePath) -> void:
-	if get_node(targetpath) is Spatial:
+export(NodePath) var TargetPath : NodePath setget set_target_path
+func set_target_path(targetpath : NodePath) -> void:
+	if not targetpath.is_empty() and get_node(targetpath) is Spatial:
 		TargetPath = targetpath
 		if not Engine.editor_hint: Target = get_node(targetpath)
 
@@ -16,6 +16,7 @@ func set_target(targetpath : NodePath) -> void:
 func _ready():
 	add_to_group("enemies", true)
 	
+	set_target_path(TargetPath)
 	if Target == null: Target = seek()
 
 
@@ -23,12 +24,12 @@ func seek() -> Spatial:
 	var target : Spatial = null
 	for player in get_tree().get_nodes_in_group("players"):
 		if target == null or translation.distance_to(player.translation) < translation.distance_to(target.translation):
-			target = target
+			target = player
 	return target
 
 
 func _process(delta):
 	if is_instance_valid(Target):
-		look_at(Target.get_transform().origin, Vector3(0,1,0))
+		look_at(Target.translation, Vector3(0,1,0))
 		rotation.z = 0
 		rotation.x = 0
