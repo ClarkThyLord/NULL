1e36f0d34d972ebfc757a46002d3515899b03b55
Christian Cuevas <ccpotential@gmail.com>
2020-04-07 21:09:29 -0700

    added music and sound effects
    Clark was here :V

--- scenes/MainMenu/MainMenu.gd
+++ scenes/MainMenu/MainMenu.gd
@@ -2,11 +2,19 @@ extends ColorRect
 
 
 
+# Refrences
+onready var MenuParticles := get_node("MenuParticles")
+
+
+
 # Core
 func _ready(): correct()
 
 
-func correct() -> void: pass
+func correct() -> void:
+	if MenuParticles:
+		MenuParticles.position = rect_size / 2
+		MenuParticles.lifetime = 4 * (rect_size.x / 1024)
 
 
 func _on_resized(): correct()
