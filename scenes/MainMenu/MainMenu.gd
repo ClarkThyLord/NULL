a598b86df762648c2a221ebdbbcd0871ff7aa1f8
Christian Cuevas <ccpotential@gmail.com>
2020-04-07 20:16:52 -0700

    reworking MainMenu
    Clark was here :V

--- scenes/MainMenu/MainMenu.gd
+++ scenes/MainMenu/MainMenu.gd
@@ -1,24 +1,12 @@
-extends Control
-
-
-
-# Refrences
-onready var Effects := get_node("Effects")
-
-onready var Start := get_node("VBoxContainer/VBoxContainer/Start")
+extends ColorRect
 
 
 
 # Core
-func _ready():
-	correct()
+func _ready(): correct()
 
 
-func correct() -> void:
-	if Effects:
-		Effects.position.x = rect_size.x / 2
-		Effects.scale.x = rect_size.x / 1024
-		Effects.scale.y = rect_size.y / 600
+func correct() -> void: pass
 
 
 func _on_resized(): correct()
@@ -27,8 +15,8 @@ func _on_resized(): correct()
 func _on_Start_pressed():
 	get_tree().change_scene("res://maps/Testing.tscn")
 
-func _on_Start_mouse_entered():
-	Start.material.blend_mode = BLEND_MODE_ADD
+func _on_Controls_pressed():
+	pass # Replace with function body.
 
-func _on_Start_mouse_exited():
-	Start.material.blend_mode = BLEND_MODE_MIX
+func _on_HighScores_pressed():
+	pass # Replace with function body.
