dbf358e31d09ca215ea182c4756c87f8cd76a584
Christian Cuevas <ccpotential@gmail.com>
2020-04-11 14:00:33 -0700

    control sound from options
    Clark was here :V

--- scenes/Options/Options.gd
+++ scenes/Options/Options.gd
@@ -18,7 +18,7 @@ func correct() -> void:
 	if popup and popup.visible:
 		var ratio = get_viewport().size / Vector2(1024, 600)
 		popup.rect_position = Vector2(212, 100) * ratio
-		popup.rect_size = popup.rect_min_size * ratio
+		popup.rect_size = Vector2(600, 400) * ratio
 
 
 func show() -> void:
@@ -34,3 +34,13 @@ func _on_Popup_visibility_changed():
 		background.visible = popup.visible
 
 func _on_Close_pressed(): hide()
+
+
+func _on_slider_value_changed(value):
+	$SoundEffects.play()
+
+
+func _on_Sound_value_changed(value):
+	value = -40 + (40 * (value * 0.01))
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), value == -40)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
