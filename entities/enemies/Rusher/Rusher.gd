77d209527bf1ecc6003d0b76eed6c9cff59aec2a
FrogEmperor <43252660+FrogEmperor@users.noreply.github.com>
2020-05-18 16:44:41 -0700

    Rusher sprite and animation

--- entities/enemies/Rusher/Rusher.gd
+++ entities/enemies/Rusher/Rusher.gd
@@ -2,6 +2,7 @@ extends "res://entities/enemies/Enemy.gd"
 
 # Refrences
 onready var HitArea := get_node("HitArea")
+onready var animation := get_node("AnimationPlayer")
 
 # Declarations
 export(int, 0, 100) var Speed := 10
@@ -9,7 +10,7 @@ export(int, 0, 100) var Speed := 10
 var hit_frames = 0
 export(int, 1, 1000) var HitRate := 100
 var attack_frames = 0
-export(int, 1, 1000) var AttackReach := 50
+export(int, 1, 1000) var AttackReach := 25
 
 export(int, 0, 100) var MinDamage := 30
 export(int, 0, 100) var MaxDamage := 40
@@ -22,16 +23,19 @@ func _ready():
 	
 func _physics_process(delta):
 	hit_frames = hit_frames + 1
-	
 	if !attack:
 		if hit_frames > HitRate:
+			animation.play("moving")
 			if translation.distance_to(Target.translation) > 25:
 				move_and_slide(Vector3.FORWARD.rotated(Vector3.UP, rotation.y) * Speed)
 			else:
 				attack = true
 				look_at_player = false
+		else:
+			animation.play("idle")
 	else:
-		move_and_slide(Vector3.FORWARD.rotated(Vector3.UP, rotation.y) * 35)
+		animation.play("attack")
+		move_and_slide(Vector3.FORWARD.rotated(Vector3.UP, rotation.y) * 70)
 		for body in HitArea.get_overlapping_bodies():
 			if body.is_in_group("players"):
 				hit(body, MinDamage + randi() % MaxDamage)
